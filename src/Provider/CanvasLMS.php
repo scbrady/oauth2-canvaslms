5eda584352b3abc6141541468e6a56009232e20d
Scott Brady <scott@prolio.com>
2018-05-30 12:39:16 -0400

    Newest oauth2 version

--- src/Provider/CanvasLMS.php
+++ src/Provider/CanvasLMS.php
@@ -12,9 +12,7 @@ use smtech\OAuth2\Client\Provider\CanvasLMSResourceOwner;
 
 class CanvasLMS extends AbstractProvider
 {
-
-    use ArrayAccessorTrait,
-        BearerAuthorizationTrait;
+    use ArrayAccessorTrait, BearerAuthorizationTrait;
 
     /**
      * URL of Canvas Instance (e.g. https://canvas.instructure.com)
@@ -29,12 +27,11 @@ class CanvasLMS extends AbstractProvider
     protected $purpose;
 
     /**
-     * Type of token requested (`authorization_code` or `refresh_token`)
-     * @var [type]
+     * Returns authorization parameters based on provided options.
+     *
+     * @param  array $options
+     * @return array Authorization parameters
      */
-    protected $grantType;
-    /* TODO https://github.com/smtech/oauth2-canvaslms/issues/1 */
-
     protected function getAuthorizationParameters(array $options)
     {
         $options = parent::getAuthorizationParameters($options);
@@ -42,26 +39,63 @@ class CanvasLMS extends AbstractProvider
         return $options;
     }
 
+    /**
+     * Returns the base URL for authorizing a client.
+     *
+     * Eg. https://oauth.service.com/authorize
+     *
+     * @return string
+     */
     public function getBaseAuthorizationUrl()
     {
         return "{$this->canvasInstanceUrl}/login/oauth2/auth";
     }
 
+    /**
+     * Returns the base URL for requesting an access token.
+     *
+     * Eg. https://oauth.service.com/token
+     *
+     * @param array $params
+     * @return string
+     */
     public function getBaseAccessTokenUrl(array $params)
     {
         return "{$this->canvasInstanceUrl}/login/oauth2/token";
     }
 
+    /**
+     * Returns the URL for requesting the resource owner's details.
+     *
+     * @param AccessToken $token
+     * @return string
+     */
     public function getResourceOwnerDetailsUrl(AccessToken $token)
     {
         return "{$this->canvasInstanceUrl}/api/v1/users/self";
     }
 
+    /**
+     * Returns the default scopes used by this provider.
+     *
+     * This should only be the scopes that are required to request the details
+     * of the resource owner, rather than all the available scopes.
+     *
+     * @return array
+     */
     public function getDefaultScopes()
     {
         return [];
     }
 
+    /**
+     * Checks a provider response for errors.
+     *
+     * @throws IdentityProviderException
+     * @param  ResponseInterface $response
+     * @param  array|string $data Parsed response data
+     * @return void
+     */
     public function checkResponse(ResponseInterface $response, $data)
     {
         if (!empty($data['error'])) {
@@ -69,11 +103,25 @@ class CanvasLMS extends AbstractProvider
         }
     }
 
+    /**
+     * Generates a resource owner object from a successful resource owner
+     * details request.
+     *
+     * @param  array $response
+     * @param  AccessToken $token
+     * @return \smtech\OAuth2\Client\Provider\CanvasLMSResourceOwner
+     */
     public function createResourceOwner(array $response, AccessToken $token)
     {
         return new CanvasLMSResourceOwner($response);
     }
 
+    /**
+     * Returns a prepared request for requesting an access token.
+     *
+     * @param array $params Query string parameters
+     * @return \Psr\Http\Message\RequestInterface
+     */
     public function getAccessTokenRequest(array $params)
     {
         $request = parent::getAccessTokenRequest($params);
